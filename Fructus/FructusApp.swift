f0e5b4f8dc50ddcf7f7782476eebfe51036605a3
Matheus Rocco <62848343+msrocco@users.noreply.github.com>
2023-09-10 16:03:43 -0400

    Initial Commit

---
+++ Fructus/FructusApp.swift
@@ -0,0 +1,17 @@
+//
+//  FructusApp.swift
+//  Fructus
+//
+//  Created by Matheus Rocco on 2023-09-10.
+//
+
+import SwiftUI
+
+@main
+struct FructusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
